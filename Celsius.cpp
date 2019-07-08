5edf18dcf1baa51635489868d7cdd9110b405f87
NJ Atcharapun <52194496+Atcharapun@users.noreply.github.com>
2019-07-08 22:01:02 +0700

    Add files via upload

---
+++ Celsius.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+
+{
+	int c;
+	cout << "Enter Celsius tempereture : ";
+	cin >> c;
+	cout << "Fahrenheit = " << ( 1.8 * c) + 32 << endl;
+	cout << "Now weather in thai is " << (((1.8 * c) + 32) >= 70? "HOT" : "COOL");
+	cout << endl;
+}
